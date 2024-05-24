9c5e1700de5b427e751b4042fc7e2d8e4a3e66bb
atkhssn <atik.hassan@outlook.com>
2024-05-24 11:08:49 +0600

    OOP One to Many Department Student

---
+++ OOP University Management/University Management/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_University_Management.University_Management
+{
+    public class Student
+    {
+        public int RegNo { get; set; }
+        public string Name { get; set; }
+    }
+}
